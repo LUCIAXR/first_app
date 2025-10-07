b16bdb39993622c6523c6e7aecc59c3fa0aaba14
LUCIAXR <maneeniyom_t@silpakorn.edu>
2025-10-07 21:48:07 +0700

    Assignment-Week5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'assignment2.dart';
-import 'dart:convert';
+import 'AssignmentWeek5.dart'; // import ไฟล์ที่มี ApiExampleList อยู่
 
 void main() {
   runApp(const MyApp());
@@ -12,17 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Vapor AQI Dashboard', // ชื่อแอพสไตล์เฟี้ยวๆ
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // ตั้งค่าให้ใช้ Light Mode ที่สดใส เพื่อรองรับดีไซน์ Vibrant
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal, // ใช้สี Teal เป็นสีฐานที่ดูสดใส
-          brightness: Brightness.light, // เปลี่ยนเป็น Light Mode เพื่อให้พื้นหลังสีขาวเข้ากับ Gradient
-        ),
-        useMaterial3: true,
-      ),
-      home: const AqiMonitorScreen(),
+      debugShowCheckedModeBanner: false, // เอาแถบ debug ออก
+      title: 'Flutter API Demo',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const Assignmentweek5(),
     );
   }
 }
