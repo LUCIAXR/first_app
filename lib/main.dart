310919decc95355531c089090655abfe9ceebe2d
LUCIAXR <maneeniyom_t@silpakorn.edu>
2025-11-05 20:51:33 +0700

    traffic_light_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,23 @@
 import 'package:flutter/material.dart';
-import 'AssignmentWeek6.dart'; // เรียกใช้ไฟล์ AssignmentWeek6.dart
+import 'package:first_app/traffic_light_screen.dart'; 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Custom Widget ProfileCard',
+      title: 'Traffic Light Animation',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Custom Widget ProfileCard'),
-        ),
-        body: Center(
-          child: ProfileCard(
-            name: 'Techawat Maneeniyom',
-            position: 'Programmer Rookie',
-            email: 'Maneeniyom_t@silpakorn.edu',
-            phoneNumber: '0802348293',
-            imageUrl: 'https://scontent.fbkk7-3.fna.fbcdn.net/v/t39.30808-6/302726068_1414419302418230_8064932503394980913_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHEWai9OoSJ8uzhuQ5fWKCcdg7rRkqNcFp2DutGSo1wWrWMi2tRODWPuhmPvsbYCNzOgNC2rc1FrzMs5xwCU6V9&_nc_ohc=EDrBxnleLK8Q7kNvwGQuKdy&_nc_oc=AdnfGu_SIbctOvau8MsldbR6P2zWv702ehZos7C66AxQtrvbcQIZeb8SmKEUIhOJnZY&_nc_zt=23&_nc_ht=scontent.fbkk7-3.fna&_nc_gid=TRIBzC8jDWJQ7Whe2vUFUg&oh=00_Afdavy4fDzlKppn_zko9SR12CrviEyh0eKsIC_i2UXt8NA&oe=68F8E417',
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: const TrafficLightScreen(),
     );
   }
 }
